4f271d1d7c2f68d9ccfcff20bae536bf34fa9606
Agus Prayogi <agus21apy@gmail.com>
2024-11-23 15:19:05 +0000

    [ADD] pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "whatsapp-api",  // Application name
+      script: "./dist/src/main.js",       // Path to your entry file
+      instances: "max",         // Run as many instances as CPU cores
+      exec_mode: "cluster",     // Enable cluster mode
+    }
+  ]
+}
