9bffbed7486271e55cdc153410acf72cf22a02cf
Agus Prayogi <agus21apy@gmail.com>
2024-11-25 22:19:58 +0700

    [CHg] type exec_mode with none

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'whatsapp-api', // Application name
       script: './dist/src/main.js', // Path to your entry file
       instances: 'max', // Run as many instances as CPU cores
-      exec_mode: 'fork', // Enable cluster mode
+      exec_mode: 'none', // Enable cluster mode
     },
   ],
 };
